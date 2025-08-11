1e3a5c41e737213559d0ca016cc679a33217a814
gokulakrishna.h <gokulakrishna.h@zohocorp.com>
2025-08-12 00:28:02 +0530

    moved to public folder

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,4 +1,19 @@
+import { useEffect, useState } from "react"
+import type { ProductIndex } from "../Constants/ProductConstants"
+
 const Home = () => {
+
+  const [latheMachines, setLatheMachines] = useState<ProductIndex[]>([]);
+
+  useEffect(() => {
+    const fetchLatheMachineData = async() => {
+      const response = await fetch("/Files/latheMachines.json");
+      const data = await response.json();
+      setLatheMachines(data);
+    }
+    fetchLatheMachineData();
+  },[])
+
   return (
     <div>Home</div>
   )
