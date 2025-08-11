bddda20204a9689cfb5485d32f742dfff4c03493
gokulakrishna.h <gokulakrishna.h@zohocorp.com>
2025-08-11 14:18:18 +0530

    Groundwork structuring

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,15 @@
 import './App.css'
+import Footer from './components/Footer'
+import Home from './components/Home'
+import NavBar from './components/NavBar'
 
 function App() {
 
   return (
     <>
+    <NavBar />
+    <Home />
+    <Footer />
     </>
   )
 }
